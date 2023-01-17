e5da896fa3dc697d2dbbcd0069f132cb0b3f35de
abdelrahmanmaherSolimanElged <AbdelrahmanMaherSolimanElged@outlook.com>
2023-01-17 16:07:45 +0200

    Add project files.

---
+++ Email/Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Email.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(MailFields email)
+        {
+            _emailService.SendEmail(email);
+            return Ok();
+        }
+    }
+
+}
+
